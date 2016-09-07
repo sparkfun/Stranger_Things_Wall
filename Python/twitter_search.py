0a45a4b942194984fd7db282df3fc03f86515a89
root <root@ubuntu.(none)>
2016-09-07 19:10:08 +0000

    Add serial

--- Python/twitter_search.py
+++ Python/twitter_search.py
@@ -10,31 +10,37 @@ sft = re.compile('#sparkfunthings')
 myPort = serial.Serial('/dev/ttyUSB0', 115200, timeout = 10)
 myPort.write("Hello, world!")
 
-try:
-    tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tso.set_keywords(['#sparkfunthings']) # let's define all words we would like to have a look for
-    tso.set_include_entities(False) # and don't give us all those entity information
+tweet_id_list = []
 
-    # it's about time to create a TwitterSearch object with our secret tokens
-    ts = TwitterSearch(
-        consumer_key = twitter_consumer_key,
-        consumer_secret = twitter_consumer_secret,
-        access_token = twitter_access_token,
-        access_token_secret = twitter_access_token_secret
-     )
+while True:
+	try:
+		print "Searching..."
+		tso = TwitterSearchOrder() # create a TwitterSearchOrder object
+		tso.set_keywords(['#sparkfunthings']) # let's define all words we would like to have a look for
+		tso.set_include_entities(False) # and don't give us all those entity information
 
-     # this is where the fun actually starts :)
-    for tweet in ts.search_tweets_iterable(tso):
-	no_hashtag = sft.sub('',  tweet['text'])
-	text_only = regex.sub('', no_hashtag).encode('ascii', 'ignore')
-	print type(text_only)
-	for character in text_only:
-		print character
-		myPort.write(character)
-		sleep(0.5)
-##        print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
+	    # it's about time to create a TwitterSearch object with our secret tokens
+		ts = TwitterSearch(
+			consumer_key = twitter_consumer_key,
+			consumer_secret = twitter_consumer_secret,
+			access_token = twitter_access_token,
+			access_token_secret = twitter_access_token_secret
+		     )
 
-except TwitterSearchException as e: # take care of all those ugly errors if there are some
-    print(e)
+	     # this is where the fun actually starts :)
+		for tweet in ts.search_tweets_iterable(tso):
+			if tweet['id'] not in tweet_id_list:
+				no_hashtag = sft.sub('',  tweet['text'])
+				text_only = regex.sub('', no_hashtag).encode('ascii', 'ignore')
+				tweet_id_list.append(tweet['id'])
+				for character in text_only:
+					print character
+					myPort.write(character)
+					sleep(0.5)
+
+	except TwitterSearchException as e: # take care of all those ugly errors if there are some
+	    print(e)
+
+	sleep(60)
 
 myPort.close()
