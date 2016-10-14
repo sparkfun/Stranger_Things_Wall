5b8c3e7fe4c1c9eb964469f45a1915bea4ee933d
Chris Westling <cmwestling@gmail.com>
2016-10-14 17:47:17 -0400

    changes to python twitter app

--- Python/credentials.py
+++
@@ -1,6 +0,0 @@
-login = {
-    'consumer_key' : 'H5nihvX92ONuJtBOlM6GB96sN',
-    'consumer_secret' : 'eoIrw47xEORbeMnMZxPwjkO3vRLcRx3EneFNQ1Oo4S1wPWDi7m',
-    'access_token' : '42788927-TI0jbg8HgUFBFap95JupbUclybUPobriILAa35lf0',
-    'access_token_secret' : 'stSKmCznYrktvLpc7yA6TKtCQxYRsJzS87exvWj0VAw7o'
-}
