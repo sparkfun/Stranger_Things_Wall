921e146058b7f7e551f8d42ac700003322c46c03
Colleen Kimball <colleenrkimball@gmail.com>
2016-10-26 20:53:37 -0400

    Update twitter_search_tweepy.py
    Added comments

--- Python/twitter_search_tweepy.py
+++ Python/twitter_search_tweepy.py
@@ -1,11 +1,29 @@
+'''
+This file uses the tweepy library to stream all public tweets with the hashtag
+#halloween.. I probably should change it so the hashtag to search for can be
+entered by the user but I haven't done that yet.
+
+To run this code you need a 'cred.yml' file in a sibling directory named 'conf'
+This yml file needs four entries:
+consumer_key
+consumer_secret
+access_token
+access_token-secret
+
+yaml format is super easy. Learn about it here: http://www.yaml.org/
+
+'''
+
 import tweepy
 import yaml
 
 conf = yaml.load(open('conf/cred.yml'))
 
+#override the tweepy stream listener class 
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
+        #when you retrieve a status, print it out
         print(status.text)
 
     def on_error(self, status_code):
@@ -14,6 +32,7 @@ class MyStreamListener(tweepy.StreamListener):
             return False
 
 def main():
+    #authorize the connection to twitter
     auth=tweepy.OAuthHandler(conf['consumer_key'], conf['consumer_secret'])
     auth.set_access_token(conf['access_token'], conf['access_token_secret'])
 
@@ -21,6 +40,7 @@ def main():
 
     while(True):
         try:
+            #stream tweets indefinitely
             myStreamListener = MyStreamListener()
             myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
             myStream.filter(track=['#halloween'])
